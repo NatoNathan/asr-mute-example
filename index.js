c24ada1e3ab55e48a93da4c4ab1ee0f8f9170189
N Tamez <nathan.tamez@vonage.com>
2022-09-26 14:42:58 +0100

    WIP

--- index.js
+++ index.js
@@ -96,9 +96,9 @@ const rtcEvent = async (event, { logger, csClient }) => {
 const voiceAnswer = async (req, res, next) => {
     const { config, logger, storageClient } = req.nexmo;
     const from = req.body.from;
-    logger.info(req.body,'prod_test_body')
+    logger.info({body:req.body},'prod_test_body')
         const user = await storageClient.get(from);
-    logger.info(user,'prod_test_user')
+    logger.info({user},'prod_test_user')
         
 
 
