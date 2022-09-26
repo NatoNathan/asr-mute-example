779e6fcf1143cfd0b7983911e288ea59fb67c86f
N Tamez <nathan.tamez@vonage.com>
2022-09-26 11:40:50 +0100

    server client

--- index.js
+++ index.js
@@ -183,9 +183,9 @@ const voiceEvent = async (req, res, next) => {
  */
 const route = (app, express) => {
 
-    if (process.env.NODE_ENV != 'production') {
-        app.use(express.static(path.join(__dirname, "public")));
-    }
+    // if (process.env.NODE_ENV != 'production') {
+    app.use(express.static(path.join(__dirname, "public")));
+    // }
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "public", "index.html"));
     });
